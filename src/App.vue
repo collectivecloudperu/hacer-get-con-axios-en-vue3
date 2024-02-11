512fc950a4f1fcf8489cf0b4c222f920d7f52298
collectivecloudperu <collectivecloudperu@gmail.com>
2024-02-11 15:27:49 -0500

    Cambios

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>  
+
+  <!-- Contenedor para la imagen -->
+  <img v-bind:src="img" class="img-fluid">
+
+</template>
+
+<script setup>
+  
+  // Importamos axios 
+  import axios from 'axios';
+
+  // Importamos 'ref' de Vue 3
+  import {ref} from "vue"
+  
+  // Hacemos la variable 'img' reactiva 
+  const img = ref(null)
+  
+  // Hacemos una petición a la Dog API 
+  axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(function (response) {
+
+      // Mostramos los datos obtenidos en la consola 
+      console.log(response);
+
+      // Accedemos a una imagen aleatoria de un perro 
+      img.value = response.data.message;
+      
+      // Mostramos la imagen aleatoria obtenida de un perro en la consola 
+      console.log(img);
+  
+    })
+    .catch(function (error) {
+  
+      // Si hubo algun error mostramos algo 
+      console.log(error);
+  
+    })
+    .finally(function () {
+  
+      // Se ejecuto sin problemas 
+      
+    });
+ 
+</script>
+
+<style>
+  /* */
+</style>
